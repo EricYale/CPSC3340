d13bd25bd60656483f655c0ceb8aafead58fb2f0
Eric Yoon <emjyoon@gmail.com>
2025-09-04 15:23:42 -0400

    Copy config script

---
+++ pi_config/copy_config.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+IP_ADDR=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+
+# cp /boot/firmware/config.txt "$SCRIPT_DIR/config.txt"
+
+cat > "$SCRIPT_DIR/ip.md" <<'EOF'
+```
+                                        /$$
+                                       |__/
+  /$$$$$$  /$$$$$$   /$$$$$$$  /$$$$$$  /$$
+ /$$__  $$|____  $$ /$$_____/ /$$__  $$| $$
+| $$  \__/ /$$$$$$$|  $$$$$$ | $$  \ $$| $$
+| $$      /$$__  $$ \____  $$| $$  | $$| $$
+| $$     |  $$$$$$$ /$$$$$$$/| $$$$$$$/| $$
+|__/      \_______/|_______/ | $$____/ |__/
+                             | $$          
+                             | $$          
+                             |__/          
+```
+
+Hello, Eric.
+
+EOF
+
+echo "My IP is \`$IP_ADDR\`." >> "$SCRIPT_DIR/ip.md"
+
+cd "$SCRIPT_DIR"
+git pull
+git add .
+git commit -m "[Auto commit] Config backup"
+git push
